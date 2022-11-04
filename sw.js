4f2748105ac52a275238342dcdf3446c537d7c7b
Nephyrr <lucasperez702@gmail.com>
2022-11-04 15:21:01 +0100

    update code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-dog-site-v1";
+const staticDevDogs = "dev-dog-site-v1";
 const assets = [
   "/",
   "/index.html",
@@ -17,7 +17,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticDevDogs).then(cache => {
       cache.addAll(assets);
     })
   );
